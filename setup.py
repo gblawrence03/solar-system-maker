ecf20d9ce02554b436f2be5d79d913accdafbfb3
George Lawrence <georgeblawrence@hotmail.co.uk>
2021-07-29 00:56:36 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name="Solar System Maker",
+      version="1.0",
+      description="Minimalistic, educational 2D solar system simulation software",
+      author="George Lawrence",
+      author_email="georgeblawrence@hotmail.co.uk",
+      url="https://github.com/gblawrence03/solar-system-maker",
+      packages=['pygame, cx_Oracle'])
